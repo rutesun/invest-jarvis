5b457a7ffd45cfc459aa21753de75e103f31a743
junhyoun.song <junhyoun.song@linecorp.com>
2026-04-09 11:47:59 +0900

    chore: bump version to 0.3.0

--- src/cli/main.py
+++ src/cli/main.py
@@ -26,7 +26,7 @@ console = Console()
 
 def version_callback(value: bool):
     if value:
-        console.print("invest-jarvis version 0.2.0")
+        console.print("invest-jarvis version 0.3.0")
         raise typer.Exit()
 
 
